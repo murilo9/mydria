0f4770390ea29ba44b0cef2006f69f104eb27771
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2020-07-12 19:25:32 -0300

    Estrutura inicial do front-end

---
+++ mydria-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Provider } from 'react-redux'
+import store from './store'
+import actions from './actions'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function App() {
+  return (
+    <Container>
+      <Row className="row">
+        <Col xs={12}>
+          <h1>My New React Bootstrap SPA</h1>
+          <Button variant="primary">Look, I'm a button!</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
