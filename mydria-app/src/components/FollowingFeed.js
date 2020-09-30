40468a1ab80c889a3ce2ad18a0ab294818ce7f0a
murilo9 <murilomatias@alunos.utfpr.edu.br>
2020-09-30 01:04:44 -0300

    Implementado FollowingFeed

--- mydria-app/src/components/FollowingFeed.js
+++ mydria-app/src/components/FollowingFeed.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
 import request from '../services/request.js';
+import { connect } from 'react-redux';
 
-import Row from 'react-bootstrap/esm/Row';
+import ListGroup from 'react-bootstrap/esm/ListGroup';
 import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+import ProfilePicture from './ProfilePicture.js';
 
-export default class Post extends Component {
+const mapStateToProps = state => ({
+  ...state
+})
+
+class FollowingFeed extends Component {
   
+  constructor(props){
+    super(props);
+    this.renderFollowedUsers = this.renderFollowedUsers.bind(this);
+  }
+
+  renderFollowedUsers(){
+    let following = [];
+    this.props.user.following.forEach(followedUser => {
+      following.push(
+        <ListGroup.Item key={followedUser.nickName}>
+          <ProfilePicture nickname={followedUser.nickname}
+            pictureId={followedUser.profilePicture} 
+            size="small" />
+          <a href={"/profile/" + followedUser.nickname} className="my-post-author-name">
+            <strong>{followedUser.nickname}</strong>
+          </a>
+        </ListGroup.Item>
+      )
+    })
+    return following;
+  }
+
   render() {
+    console.log(this.props.user)
     return (
-      <Col sm={3} className="my-ads d-none d-sm-flex">Following Feed</Col>
+      <Col sm={3} className="my-following-feed d-none d-sm-block">
+        <p>
+          { this.props.user.following.length ? 
+          'People you follow' : "You're not following anybody yet" }
+        </p>
+        <ListGroup variant="flush">
+          { this.renderFollowedUsers() }
+        </ListGroup>
+      </Col>
     )
   }
 }
+
+export default connect(mapStateToProps)(FollowingFeed);
