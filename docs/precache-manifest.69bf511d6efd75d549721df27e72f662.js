cc67a90bfad8ace849cbfa459d5920721ec7ddb3
murilo9 <murilomatias@alunos.utfpr.edu.br>
2020-11-20 02:37:05 -0300

    Deploy do app

---
+++ docs/precache-manifest.69bf511d6efd75d549721df27e72f662.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33e688ac7966ed72a08434eac3373d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5217b9fb189ee7eae3b0",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "c520a2be6ba3e3327de0",
+    "url": "/static/css/main.7bc4fe83.chunk.css"
+  },
+  {
+    "revision": "5217b9fb189ee7eae3b0",
+    "url": "/static/js/2.f9d5ae78.chunk.js"
+  },
+  {
+    "revision": "8e6e5cff6eb330bfe0d5e39bbad71b26",
+    "url": "/static/js/2.f9d5ae78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c520a2be6ba3e3327de0",
+    "url": "/static/js/main.ec56f093.chunk.js"
+  },
+  {
+    "revision": "e27fcbfc6148658f3fcd",
+    "url": "/static/js/runtime-main.fe71d981.js"
+  },
+  {
+    "revision": "03a7f67c33c44897db76fa072725df8d",
+    "url": "/static/media/Alata-Regular.03a7f67c.ttf"
+  },
+  {
+    "revision": "bea35746fb7cdd67b15ecf0622d8cc85",
+    "url": "/static/media/Kalam-Regular.bea35746.ttf"
+  },
+  {
+    "revision": "78b633116230c760b4beb97a22c2e700",
+    "url": "/static/media/login-background.78b63311.jpg"
+  }
+]);
